be6733de8338d04f3b59c2cf91ab1d3e044da4be
Chirag Ahuja <cahuja1992@gmail.com>
2017-10-27 23:53:42 +0530

    docker-compose initiated

---
+++ build-dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Build based docker container
+docker build --build-arg="proxy=$http_proxy" -t destination-pred/python -f PythonDepDockerFile .
+
+# Build add docker container
+docker build -t destination-pred/app -f Dockerfile .
