24d975a5c22e14c2571ccccfffb31124f2c35523
cahuja <cahuja@visteon.com>
2017-10-30 15:35:52 +0530

    packed as a command line arguments

---
+++ src/app.py
@@ -0,0 +1,45 @@
+import getopt
+import sys
+
+from models import MLPModel
+from preprocessor.clustering import MeanShiftClustering
+from preprocessor.loader import Data
+
+
+def arg_parser():
+    params = dict()
+    myopts, args = getopt.getopt(sys.argv[1:], "i:o:")
+    params['is_train'] = False
+
+    for o, a in myopts:
+        if o == '-m':
+            params['model_name'] = a
+        elif o == '-t':
+            params['is_train'] = a
+        else:
+            print("Usage: %s -m input -o output" % sys.argv[0])
+
+    return params
+
+
+if __name__ == '__main__':
+    params = arg_parser()
+    model_name = params['model_name']
+    if params['is_train']:
+        print(" DESTINATION PREDICTION : Training model using {} : %s".format(model_name))
+
+        data = Data()
+        data.load_data()
+
+        ms = MeanShiftClustering(data.Y_train)
+        ms.fit()
+        clusters = ms.get_cluster_centroids()
+
+        mlp = MLPModel.MLPModel()
+        mlp.set_data(data)
+        mlp.set_clusters(clusters)
+        mlp.create_model()
+        mlp.fit()
+        mlp.save()
+    else:
+        print('Prediction not implemented yet')
